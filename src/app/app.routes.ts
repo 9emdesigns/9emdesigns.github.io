7f89e84b8defcc14730317a1b1c59adeecab3569
Zayno Lane <zaynolane@MacBook-Pro.local>
2026-06-11 21:22:27 -0400

    website 9em pages and routes update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { AboutUsComponent } from './component/about-us/about-us.component';
+import { DesignConceptComponent } from './component/design-concept/design-concept.component';
+import { WhatWeDoComponent } from './component/what-we-do/what-we-do.component';
+import { HowWeWorkComponent } from './component/how-we-work/how-we-work.component';
+import { ContactUsComponent } from './component/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'aboutUs',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'designConcept',
+    component: DesignConceptComponent,
+  },
+  {
+    path: 'whatWeDo',
+    component: WhatWeDoComponent,
+  },
+  {
+    path: 'howWeWork',
+    component: HowWeWorkComponent,
+  },
+  {
+    path: 'contactUs',
+    component: ContactUsComponent,
+  },
+];
