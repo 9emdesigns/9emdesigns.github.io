9a454b4c081054ab191acab421ceed38a96ffd4c
Zayno Lane <zaynolane@MacBook-Pro.local>
2026-09-14 20:49:20 -0400

    added SSR

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-NPNQPERS.mjs";import"./chunk-VSS4USQ6.mjs";import"./chunk-7HCEC7IM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,5 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideClientHydration(), provideClientHydration()]
 };
